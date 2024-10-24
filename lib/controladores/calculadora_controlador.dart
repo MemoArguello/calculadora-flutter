011d652e64cbf46ba43899e75e45218aaf2c93d0
Guillermo Arguello <elmemozac6@gmail.com>
2024-10-24 19:47:14 -0300

    correccion

--- lib/controladores/calculadora_controlador.dart
+++ lib/controladores/calculadora_controlador.dart
@@ -1,95 +1,93 @@
 import "package:get/get.dart";
 
-class CalculadoraControlador extends GetxController{
-
+class CalculadoraControlador extends GetxController {
   var firstNumber = '0'.obs;
   var secondNumber = '0'.obs;
   var mathResult = '0'.obs;
   var operation = '+'.obs;
 
-  resetAll(){
+  resetAll() {
     firstNumber.value = '0';
     secondNumber.value = '0';
     mathResult.value = '0';
     operation.value = '+';
   }
 
-  cambiarNegativoPositivo(){
-    if(mathResult.startsWith('-')){
-      mathResult.value = mathResult.value.replaceFirst('-','');
-    }else{
+  cambiarNegativoPositivo() {
+    if (mathResult.startsWith('-')) {
+      mathResult.value = mathResult.value.replaceFirst('-', '');
+    } else {
       mathResult.value = '-${mathResult.value}';
     }
   }
 
-  addNumber( String number ) {
-
-    if( mathResult.value == '0'){
+  addNumber(String number) {
+    if (mathResult.value == '0') {
       return mathResult.value = number;
     }
 
-    if( mathResult.value == '-0'){
+    if (mathResult.value == '-0') {
       return mathResult.value = '-$number';
     }
 
     mathResult.value = mathResult.value + number;
   }
 
-  addDecimalPoint(){
+  addDecimalPoint() {
+    if (mathResult.contains('.')) return;
 
-    if(mathResult.contains('.')) return;
-
-    if(mathResult.startsWith('0')){
+    if (mathResult.startsWith('0')) {
       mathResult.value = '0.';
-    }else{
+    } else {
       mathResult.value = '${mathResult.value}.';
     }
   }
 
-  selectOperation( String newOperation){
+  selectOperation(String newOperation) {
     operation.value = newOperation;
     firstNumber.value = mathResult.value;
     mathResult.value = '0';
   }
 
-  deleteLastEntry(){
-    if(mathResult.value.replaceAll('-','').length>1){
-      mathResult.value = mathResult.value.substring(0,mathResult.value.length - 1);
-    }else{
+  deleteLastEntry() {
+    if (mathResult.value.replaceAll('-', '').length > 1) {
+      mathResult.value =
+          mathResult.value.substring(0, mathResult.value.length - 1);
+    } else {
       mathResult.value = '0';
     }
   }
 
-  calculateResult(){
-
+  calculateResult() {
     double num1 = double.parse(firstNumber.value);
     double num2 = double.parse(mathResult.value);
 
     secondNumber.value = mathResult.value;
 
-    switch( operation.value ){
-      case'+':
-        mathResult.value = '${ num1 + num2 }';
-      break;
+    switch (operation.value) {
+      case '+':
+        mathResult.value = '${num1 + num2}';
+        break;
 
-      case'-':
-        mathResult.value = '${ num1 + num2 }';
-      break;
+      case '-':
+        mathResult.value = '${num1 - num2}';
+        break;
 
-      case'/':
-        mathResult.value = '${ num1 + num2 }';
-      break;
-      
-      case'X':
-        mathResult.value = '${ num1 + num2 }';
-      break;
+      case '/':
+        mathResult.value = '${num1 / num2}';
+        break;
+
+      case 'X':
+        mathResult.value = '${num1 * num2}';
+        break;
 
       default:
-      return;
+        return;
     }
 
-    if ( mathResult.value.endsWith('.0')){
-      mathResult.value = mathResult.value.substring(0, mathResult.value.length - 2);
+    if (mathResult.value.endsWith('.0')) {
+      mathResult.value =
+          mathResult.value.substring(0, mathResult.value.length - 2);
     }
   }
 }
