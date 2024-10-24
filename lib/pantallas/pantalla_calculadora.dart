529b7434459348d292352befb750c693608e7960
Guillermo Arguello <elmemozac6@gmail.com>
2024-10-24 19:13:02 -0300

    cambios03

--- lib/pantallas/pantalla_calculadora.dart
+++ lib/pantallas/pantalla_calculadora.dart
@@ -54,12 +54,136 @@ class PantallaCalculadora extends StatelessWidget {
                   ),
                 ],
               ),
-            ],
-          ),
-        ),
 
-        //segu
+                // Segunda fila de botones
+            Row(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: [
+                Expanded(
+                  child: CalculatorButton(
+                    text: '7',
+                    onPressed: () => calculatorCtrl.addNumber('7'),
+                  ),
+                ),
+                Expanded(
+                  child: CalculatorButton(
+                    text: '8',
+                    onPressed: () => calculatorCtrl.addNumber('8'),
+                  ),
+                ),
+                Expanded(
+                  child: CalculatorButton(
+                    text: '9',
+                    onPressed: () => calculatorCtrl.addNumber('9'),
+                  ),
+                ),
+                Expanded(
+                  child: CalculatorButton(
+                    text: 'X',
+                    bgColor: const Color(0xffA5A5A5),
+                    onPressed: () => calculatorCtrl.selectOperation('X'),
+                  ),
+                ),
+              ],
+            ),
+
+            // Tercera fila de botones
+            Row(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: [
+                Expanded(
+                  child: CalculatorButton(
+                    text: '4',
+                    onPressed: () => calculatorCtrl.addNumber('4'),
+                  ),
+                ),
+                Expanded(
+                  child: CalculatorButton(
+                    text: '5',
+                    onPressed: () => calculatorCtrl.addNumber('5'),
+                  ),
+                ),
+                Expanded(
+                  child: CalculatorButton(
+                    text: '6',
+                    onPressed: () => calculatorCtrl.addNumber('6'),
+                  ),
+                ),
+                Expanded(
+                  child: CalculatorButton(
+                    text: '-',
+                    bgColor: const Color(0xffA5A5A5),
+                    onPressed: () => calculatorCtrl.selectOperation('-'),
+                  ),
+                ),
+              ],
+            ),
+
+            // Cuarta fila de botones
+            Row(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: [
+                Expanded(
+                  child: CalculatorButton(
+                    text: '1',
+                    onPressed: () => calculatorCtrl.addNumber('1'),
+                  ),
+                ),
+                Expanded(
+                  child: CalculatorButton(
+                    text: '2',
+                    onPressed: () => calculatorCtrl.addNumber('2'),
+                  ),
+                ),
+                Expanded(
+                  child: CalculatorButton(
+                    text: '3',
+                    onPressed: () => calculatorCtrl.addNumber('3'),
+                  ),
+                ),
+                Expanded(
+                  child: CalculatorButton(
+                    text: '+',
+                    bgColor: const Color(0xffA5A5A5),
+                    onPressed: () => calculatorCtrl.selectOperation('+'),
+                  ),
+                ),
+              ],
+            ),
+
+            // Última fila de botones (0, ., =)
+            Row(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: [
+                Expanded(
+                  flex: 2, // Mayor espacio para el botón '0' porque es grande
+                  child: CalculatorButton(
+                    text: '0',
+                    big: true,
+                    onPressed: () => calculatorCtrl.addNumber('0'),
+                  ),
+                ),
+                Expanded(
+                  flex: 1, // Espacio para el botón '.'
+                  child: CalculatorButton(
+                    text: '.',
+                    onPressed: () => calculatorCtrl.addDecimalPoint(),
+                  ),
+                ),
+                Expanded(
+                  flex: 1, // Espacio para el botón '='
+                  child: CalculatorButton(
+                    text: '=',
+                    bgColor: const Color(0xffA5A5A5),
+                    onPressed: () => calculatorCtrl.calculateResult(),
+               ),
+                ),
+              ],
+            ),
+          ],
+        ),
       ),
-    );
-  }
+    ),
+  );
+}
 }
